2817fa70660ac42a32dbf6830298b53daaadce77
aaronvc2005 <chempolaaaron@gmail.com>
2026-07-01 21:03:54 +0800

    Delete import torch.py

--- import torch.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-# Automatically switches to GPU if available in the cloud
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Current training device: {device}")
-
-# When sending your graph mesh to the model:
-# model = HydroGraphNet().to(device)
-# data = data.to(device)
